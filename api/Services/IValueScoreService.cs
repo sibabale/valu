3c0fc42742b701fda86732af513a2b56e4af78b8
Sibabale <sibabale.joja@oldmutual.com>
2025-08-10 13:44:00 +0200

    feat: add cancellationtoken support to ivaluescoreservice async methods

--- api/Services/IValueScoreService.cs
+++ api/Services/IValueScoreService.cs
@@ -1,10 +1,11 @@
 using Valu.Api.Models;
+using System.Threading;
 
 namespace Valu.Api.Services;
 
 public interface IValueScoreService
 {
-    Task<ValueScore> CalculateValueScoreAsync(CalculateValueScoreRequest request);
-    Task<ValueScore?> GetValueScoreAsync(Guid companyId);
-    Task<IEnumerable<ValueScore>> GetTopValueStocksAsync(int count = 10);
+    Task<ValueScore> CalculateValueScoreAsync(CalculateValueScoreRequest request, CancellationToken cancellationToken = default);
+    Task<ValueScore?> GetValueScoreAsync(Guid companyId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<ValueScore>> GetTopValueStocksAsync(int count = 10, CancellationToken cancellationToken = default);
 } 
